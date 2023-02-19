7f2f1fc2cc596f0298f6bb68dabd03049e1a9d68
Luca Scalzotto <luca@scorpiac.com>
2023-02-19 15:23:31 +0100

    Add test for instances of identical services

--- test/Injector.test.ts
+++ test/Injector.test.ts
@@ -1,9 +1,25 @@
 import { Injector } from '../src/Injector';
+import { CounterService } from './services/CounterService';
+import { CounterService as CounterService2 } from './services/CounterServiceDuplicate';
 
 describe('Injector', () => {
+  let i: Injector;
+
+  beforeEach(() => {
+    i = new Injector();
+  });
+
   it('binds to itself', () => {
-    const i = new Injector();
     const i2 = i.resolve(Injector);
     expect(i === i2).toBeTruthy();
+  });
+
+  it('keeps separate instances of identical services', () => {
+    const c1 = i.resolve(CounterService);
+    const c2 = i.resolve(CounterService2);
+
+    expect(c1.counter).toEqual(c2.counter);
+    c1.counter++;
+    expect(c1.counter).not.toEqual(c2.counter);
   });
 });
---
+++ test/services/CounterService.ts
@@ -0,0 +1,3 @@
+export class CounterService {
+  public counter = 0;
+}
